b4806a2d2aa02dc9c358c293cd7d223b53ab4111
Esra <esraayaareb@gmail.com>
2024-12-14 03:06:44 +0300

    desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+
 function App() {
-  return <main></main>;
+  return (
+    <>
+      <main>
+        <img src="./resources/images/logo.svg" alt="Logo" className="logo" />
+        <h1>
+          We are launching <span>soon!</span>
+        </h1>
+
+        <h2>Subscribe and get notified</h2>
+
+        <form action="">
+          <input type="text" placeholder="Your email address…" />
+          <button type="submit">Notify Me</button>
+        </form>
+
+        <img
+          src="./resources/images/illustration-dashboard.png"
+          alt="Hero Image"
+          className="hero_image"
+        />
+
+        <div className="footer">
+          <div className="icons">
+            <a href="#">
+              <FaFacebookF />
+            </a>
+
+            <a href="#">
+              <FaTwitter />
+            </a>
+
+            <a href="#">
+              <FaInstagram />
+            </a>
+          </div>
+          <p>&copy; Copyright Ping. All rights reserved.</p>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
